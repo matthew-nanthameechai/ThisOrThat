5f5b85cbd7f94346c41cb6bc270f24490bc43e6e
Berhane Fesehaye <brhanefsehaye3@gmail.com>
2024-03-08 12:34:44 +1300

    Added css

--- client/components/ThisorThat.tsx
+++ client/components/ThisorThat.tsx
@@ -1,11 +1,19 @@
-function ThisorThat(){
+import { Link } from 'react-router-dom'
+
+function ThisorThat() {
   return (
     <>
       <div>
         <h1>This Or That Pokemon Edition</h1>
       </div>
+      <Link to="/some-route">
+        <input type="button" value="Type here" />
+      </Link>
+      <Link to="/some-other-route">
+        <input type="button" value="Type here" />
+      </Link>
     </>
   )
 }
 
-export  default ThisorThat
+export default ThisorThat
