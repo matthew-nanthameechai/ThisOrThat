37f6df1998278c991608c95f174639c76fbbe1f5
Alyssa Bruns <154466112+alyssa-bruns@users.noreply.github.com>
2024-03-08 10:10:37 +1300

    Add imports

---
+++ client/components/PokemonGame.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { useQuery } from '@tanstack/react-query'
+import { fetchPokemonByName, fetchPokemonGeneration } from '../apis/pokemon.ts'
+import {Link, useParams} from 'react-router-dom'
+
+
+
+// add imports
+// create function
+    // create query / handle api
+        // create array with images
+    // get random image function
+    // create a visible/not visible state
+    // handle click
+    
+    // create return
+        // create containers
+        // add reset button
+        // add images to containers
+            // make images clickable
+
+            
