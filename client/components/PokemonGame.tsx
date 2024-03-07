80f3839cd3dbf82444bc4ffaa775578f160a8ca4
Alyssa Bruns <154466112+alyssa-bruns@users.noreply.github.com>
2024-03-08 10:32:14 +1300

    Create PokemonGame component

--- client/components/PokemonGame.tsx
+++ client/components/PokemonGame.tsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 import { useQuery } from '@tanstack/react-query'
 import { fetchPokemonByName, fetchPokemonGeneration } from '../apis/pokemon.ts'
 import {Link, useParams} from 'react-router-dom'
+import { Pokemon } from '../../models/pokemon.ts'
+
+export default function PokemonGame () {
+    const { data: isPending, isError} = useQuery
+    
+}
 
 
-
-// add imports
 // create function
     // create query / handle api
         // create array with images
@@ -18,5 +22,3 @@ import {Link, useParams} from 'react-router-dom'
         // add reset button
         // add images to containers
             // make images clickable
-
-            
